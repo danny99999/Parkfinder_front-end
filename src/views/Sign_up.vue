b995bd89a4fbd574f57042ddca3974304808acad
Danijel Dupor <danijeldupor.dd@gmail.com>
2022-05-03 14:52:07 +0200

    frontend_commit11

--- src/views/Sign_up.vue
+++ src/views/Sign_up.vue
@@ -71,7 +71,6 @@
             <b-button class="btn2" href="/">Već imam račun</b-button>
             <br>
             <br>
-            <b-button class="btn2" href="/promjena_lozinke">Promjena lozinke</b-button>
             </b-card>
             </center>
             </form>
